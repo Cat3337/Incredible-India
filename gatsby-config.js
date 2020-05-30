b0a3d0fff27d6a392e01c6f557c1ce3f92014201
Cat3337 <66141561+Cat3337@users.noreply.github.com>
2020-05-30 11:17:31 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Incredible India`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`
